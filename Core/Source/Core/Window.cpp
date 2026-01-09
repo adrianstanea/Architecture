f4c51e990f2edea0a4230cc544d836a4d069d7be
astanea <adrianstanea1@gmail.com>
2026-01-09 16:20:45 +0200

    Core: downgrade GLFW to 4.5 for compatiblity between host and container
    - Host version was 4.6 and container runs 4.5 based on `glxinfo -B `

--- Core/Source/Core/Window.cpp
+++ Core/Source/Core/Window.cpp
@@ -20,7 +20,7 @@ namespace Core {
 	void Window::Create()
 	{
 		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
 		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 		glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
 
